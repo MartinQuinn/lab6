5a9c5c44f22385bb6792abe64664b70549e8b669
User <martoq28@gmail.com>
2015-10-21 17:05:22 +0300

    Message

---
+++ my_application/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/")
+def index():
+    return "Index File"
+
+@app.route("/user/<username>")
+def paul():
+    return "user %s" % username
+
+@app.route("/post/<int:post_id>")
+def post():
+    return "post %d" % post_id
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080,debug=True)
